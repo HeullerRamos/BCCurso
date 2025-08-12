a63f97cd9732e192e97be2e2c2443c004a921bdd
felkng <felipersf374@gmail.com>
2025-08-11 23:40:46 -0300

    Remove unnecessary code

--- src/resources/js/app.js
+++ src/resources/js/app.js
@@ -1,34 +1,30 @@
 import './bootstrap';
 
-import jQuery from 'jquery';
-window.$ = window.jQuery = jQuery;
-
 
 import 'summernote/dist/summernote-lite.js';
 import 'summernote/dist/summernote-lite.css';
 
-
 document.addEventListener('DOMContentLoaded', function() {
-  // Certifique-se de que o seletor #texto aponte para o seu <textarea>
   $('#texto').summernote({
+    lang: 'pt-BR',
     placeholder: 'Digite o conteúdo da sua postagem aqui...',
     tabsize: 2,
-    height: 300, // Altura do editor
-    // Desativa a funcionalidade de arrastar e soltar (drag-and-drop)
-    disableDragAndDrop: true, // <--- Adicione esta linha!
+    height: 300,
+    disableDragAndDrop: true,
 
     fontNames: ['Arial', 'Arial Black', 'Comic Sans MS', 'Courier New', 'Helvetica', 'Impact', 'Lucida Grande', 'Tahoma', 'Times New Roman', 'Verdana'],
     fontNamesIgnoreCheck: ['Arial Black', 'Comic Sans MS', 'Lucida Grande', 'Times New Roman'],
 
     toolbar: [
-      ['style', ['style', 'bold', 'italic', 'underline']],
-      ['font', ['strikethrough', 'superscript', 'subscript', 'bold', 'underline', 'clear']],
+      ['style', ['style']],
+      ['text', ['bold', 'italic', 'underline', 'strikethrough']],
+      ['font', ['superscript', 'subscript', 'clear']],
       ['fontname', ['fontname']],
       ['color', ['color']],
       ['para', ['ul', 'ol', 'paragraph']],
       ['table', ['table']],
-      ['insert', ['link', 'picture', 'video']],
-      ['view', ['codeview', 'help']],
+      ['insert', ['link']],
+      ['view', ['fullscreen', 'codeview', 'help']],
     ]
   });
 });
